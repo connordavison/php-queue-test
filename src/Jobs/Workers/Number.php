f83d737fc44e474b7518da8047a512cdc19435ab
C. Davison <connorpdavison@gmail.com>
2015-12-07 22:13:56 +0000

    Corrected type hint for runJob. Added result_data for the job (apparently that's mandatory).

--- src/Jobs/Workers/Number.php
+++ src/Jobs/Workers/Number.php
@@ -3,11 +3,13 @@ namespace CDavison\Jobs\Workers;
 
 class Number extends \PHPQueue\Worker
 {
-    public function runJob(\PHPQueue\Worker $queued_job)
+    public function runJob(\PHPQueue\Job $queued_job)
     {
         $number = $queued_job->data;
 
         sleep(10);
         echo $number;
+
+        $this->result_data = array('status' => 'success');
     }
 }
