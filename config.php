19bbd9fee1953d76d7f8a2c91994a5b3aacf7b8a
C. Davison <connorpdavison@gmail.com>
2015-12-07 22:12:45 +0000

    Add namespace definitions for PHPQueue.

--- config.php
+++ config.php
@@ -2,3 +2,6 @@
 define('PID_FILE',    __DIR__ . DIRECTORY_SEPARATOR . '.pid');
 define('STDOUT_FILE', __DIR__ . DIRECTORY_SEPARATOR . '.stdout');
 define('STDERR_FILE', __DIR__ . DIRECTORY_SEPARATOR . '.stderr');
+
+define('QUEUE_NAMESPACE',  '\CDavison\Jobs\Queues');
+define('WORKER_NAMESPACE', '\CDavison\Jobs\Workers');
